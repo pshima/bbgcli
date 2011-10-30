1b715499d815af836dcee428e1dc957e5f803bd6
Jerry Chen <jerry@apache.org>
2011-10-30 06:30:31 -0500

    Move BBGAPI classes into separate files

--- lib/bbgapi.rb
+++ lib/bbgapi.rb
@@ -2,6 +2,9 @@ require 'httparty'
 require 'highline'
 require 'highline/import'
 require 'pp'
+require 'bbgapi/lb_applications'
+require 'bbgapi/lb_backends'
+require 'bbgapi/lb_services'
 
 module BBGAPI
 
@@ -21,7 +24,7 @@ module BBGAPI
       out.write(a.to_yaml)
     end
     @@bluebox_config = YAML.load_file('./bluebox.yaml')
-  end 
+  end
 
   def self.config
     @@bluebox_config
@@ -64,231 +67,6 @@ module BBGAPI
     puts "bla"
   end
 
-  class LB_Applications
-
-    def self.menulist
-        choose do |menu|
-          puts "Load Balancer Applications"
-          puts "----------------------------------------"
-          menu.prompt = "Which Action?"
-
-          menu.choices(:list) {self.list}
-          menu.choices(:create) {self.tbi}
-          menu.choices(:update) {self.tbi}
-          menu.choices(:delete) {self.tbi}
-        end
-    end
-
-    def self.list
-      partial = '/api/lb_applications'
-      api_response = BBGAPI::geturl(partial,"")
-      apps = []
-      api_response.each {|x|
-        apps << {"name" => "#{x["name"]}","id" => "#{x["id"]}"}
-        puts "\n"
-        puts "Name:        #{x["name"]}"
-        puts "ID:          #{x["id"]}"
-        puts "Description: #{x["description"]}"
-        puts "External IP: #{x["ip_v4"]}"
-        puts "Internal IP: #{x["source_ip_v4"]}"
-        puts "Created:     #{x["created"]}"
-      }
-      puts "\n"
-
-      choose do |menu|
-        menu.prompt = "Which App Should I Set as Default?"
-        apps.each {|k|
-          menu.choices(k["name"]) {self.set_app(k["id"])}
-        }
-      end
-      BBGAPI::parseopt("lb")
-    end
-
-    def self.get_app
-      return @@current_app
-    end
-
-    def self.set_app (appid="")
-        @@current_app = appid
-    end
-
-    def self.only_name_id
-      partial = '/api/lb_applications'
-      api_response = BBGAPI::geturl(partial,"")
-      apps = []
-      api_response.each {|x|
-        apps << {"name" => "#{x["name"]}","id" => "#{x["id"]}"}
-      }
-      return apps
-    end
-
-    def self.tbi
-      puts "This is not yet implemented"
-      BBGAPI::parseopt("lb")
-    end
-
-  end
-
-  class LB_Services
-    def self.menulist
-        choose do |menu|
-          puts "Load Balancer Services"
-          puts "----------------------------------------"
-          menu.prompt = "Which Action?"
-          menu.choices(:list) {self.list}
-          menu.choices(:create) {self.tbi}
-          menu.choices(:update) {self.tbi}
-          menu.choices(:delete) {self.tbi}
-        end
-    end
-
-    def self.get_service
-      return @@current_service
-    end
-
-    def self.set_service (serviceid="")
-        @@current_service = serviceid
-    end
-
-    def self.recurse
-      apps = BBGAPI::LB_Applications.only_name_id
-      pp apps
-      exit 0
-      choose do |menu|
-        menu.prompt = "Which Application?"
-        apps.each {|k|
-          menu.choices(k["name"]) {
-            BBGAPI::LB_Applications.set_app(k["id"])
-            cur_app = k["id"]
-          }
-        }
-      end
-      return cur_app
-    end
-
-    def self.list
-      
-      begin
-        cur_app = BBGAPI::LB_Applications.get_app
-      rescue
-        cur_app = self.recurse
-      end
-
-      partial = "/api/lb_applications/#{cur_app}/lb_services"
-      api_response = BBGAPI::geturl(partial,"")
-      api_response.each {|x|
-        puts "\n"
-        puts "Name:         #{x["name"]}"
-        puts "ID:           #{x["id"]}"
-        puts "Description:  #{x["description"]}"
-        puts "Port:         #{x["port"]}"
-        puts "Service Type: #{x["service_type"]}"
-        puts "Status URL:   #{x["status_url"]}"
-        puts "Status User:  #{x["status_username"]}"
-        puts "Status Pass:  #{x["status_password"]}"
-        puts "Created:      #{x["created"]}"
-      }
-      puts "\n"
-      BBGAPI::parseopt("lb")
-    end
-
-    def self.only_name_id
-      begin
-        cur_app = BBGAPI::LB_Applications.get_app
-      rescue
-        cur_app = self.recurse
-      end
-
-      partial = "/api/lb_applications/#{cur_app}/lb_services"
-      api_response = BBGAPI::geturl(partial,"")
-      services = []
-      api_response.each {|x|
-        services << {"name" => "#{x["name"]}","id" => "#{x["id"]}"}
-      }
-      return services
-    end
-
-    def self.tbi
-      puts "This is not yet implemented"
-      BBGAPI::parseopt("lb")
-    end
-
-  end
-
-  class LB_Backends
-    def self.menulist
-        choose do |menu|
-          puts "Load Balancer Backends"
-          puts "----------------------------------------"
-          menu.prompt = "Which Action?"
-          menu.choices(:list) {self.list}
-          menu.choices(:create) {self.tbi}
-          menu.choices(:update) {self.tbi}
-          menu.choices(:delete) {self.tbi}
-        end
-    end
-
-    def self.recurse
-      apps = BBGAPI::LB_Applications.only_name_id
-      choose do |menu|
-        menu.prompt = "Which Application?"
-        apps.each {|k|
-          menu.choices(k["name"]) {
-            BBGAPI::LB_Applications.set_app(k["id"])
-            cur_app = k["id"]
-          }
-        }
-      end
-
-      services = BBGAPI::LB_Services.only_name_id
-      choose do |menu|
-        menu.prompt = "Which Service?"
-        services.each {|k|
-          menu.choices(k["name"]) {
-            BBGAPI::LB_Services.set_service(k["id"])
-            cur_service = k["id"]
-          }
-        }
-      end
-
-    end
-
-    def self.list
-      begin
-        cur_app = BBGAPI::LB_Applications.get_app
-        cur_service = BBGAPI::LB_Services.get_service
-      rescue
-        self.recurse
-        cur_app = BBGAPI::LB_Applications.get_app
-        cur_service = BBGAPI::LB_Services.get_service
-      end
-
-      partial = "/api/lb_services/#{cur_service}/lb_backends"
-      api_response = BBGAPI::geturl(partial,"")
-      api_response.each {|x|
-        puts "\n"
-        puts "Name:            #{x["backend_name"]}"
-        puts "ID:              #{x["id"]}"
-        puts "Alive Check:     #{x["monitoring_url"]}"
-        puts "Check Interval:  #{x["check_interval"]}"
-        nodes = x["lb_machines"].sort_by {|var| var["hostname"]}
-        nodes.each {|y|
-          puts "App:             #{y["hostname"]}"
-        }
-      }
-      puts "\n"
-      BBGAPI::parseopt("lb")
-    end
-
-
-
-    def self.tbi
-      puts "This is not yet implemented"
-      BBGAPI::parseopt("lb")
-    end
-
-  end
-
   def self.parseopt(type="",action="",id="")
     ssh_key_file = File.new("#{ENV['HOME']}/.ssh/id_rsa.pub", "r")
     keyfile = ssh_key_file.gets
@@ -301,37 +79,30 @@ module BBGAPI
 
 
     case type
-      when "lb"
+    when "lb"
 
-        choose do |menu|
-          puts "Load Balancer API - http://bit.ly/ucbpDF"
-          puts "----------------------------------------"
-          menu.prompt = "Which Objects Would You Like?  "
+      choose do |menu|
+        puts "Load Balancer API - http://bit.ly/ucbpDF"
+        puts "----------------------------------------"
+        menu.prompt = "Which Objects Would You Like?  "
 
-          menu.choices(:applications) {BBGAPI::LB_Applications.menulist}
-          menu.choices(:services) {BBGAPI::LB_Services.menulist}
-          menu.choices(:backends) {BBGAPI::LB_Backends.menulist}
-          menu.choices(:machines)
-          menu.choices(:exit) {exit 0}
-        end
+        menu.choices(:applications) {BBGAPI::LB_Applications.menulist}
+        menu.choices(:services) {BBGAPI::LB_Services.menulist}
+        menu.choices(:backends) {BBGAPI::LB_Backends.menulist}
+        menu.choices(:machines)
+        menu.choices(:exit) {exit 0}
+      end
 
-      when "blocks"
-        puts "blocks"
-      when "servers"
-        puts "servers"
-      when "help"
-        BBGAPI::help
-      else
-        "Invalid --api option, try --help"
+    when "blocks"
+      puts "blocks"
+    when "servers"
+      puts "servers"
+    when "help"
+      BBGAPI::help
+    else
+      "Invalid --api option, try --help"
     end
     # Squire.scribble("INFO","200 Successfully Retrieved #{type} data from Blue Box"); return response if response.code == 200
-
   end
 
-
-
-
-
-
 end
-
---
+++ lib/bbgapi/lb_backends.rb
@@ -0,0 +1,73 @@
+module BBGAPI
+  class LB_Backends
+
+    def self.menulist
+      choose do |menu|
+        puts "Load Balancer Backends"
+        puts "----------------------------------------"
+        menu.prompt = "Which Action?"
+        menu.choices(:list) {self.list}
+        menu.choices(:create) {self.tbi}
+        menu.choices(:update) {self.tbi}
+        menu.choices(:delete) {self.tbi}
+      end
+    end
+
+    def self.recurse
+      apps = BBGAPI::LB_Applications.only_name_id
+      choose do |menu|
+        menu.prompt = "Which Application?"
+        apps.each {|k|
+          menu.choices(k["name"]) {
+            BBGAPI::LB_Applications.set_app(k["id"])
+            cur_app = k["id"]
+          }
+        }
+      end
+
+      services = BBGAPI::LB_Services.only_name_id
+      choose do |menu|
+        menu.prompt = "Which Service?"
+        services.each {|k|
+          menu.choices(k["name"]) {
+            BBGAPI::LB_Services.set_service(k["id"])
+            cur_service = k["id"]
+          }
+        }
+      end
+    end
+
+    def self.list
+      begin
+        cur_app = BBGAPI::LB_Applications.get_app
+        cur_service = BBGAPI::LB_Services.get_service
+      rescue
+        self.recurse
+        cur_app = BBGAPI::LB_Applications.get_app
+        cur_service = BBGAPI::LB_Services.get_service
+      end
+
+      partial = "/api/lb_services/#{cur_service}/lb_backends"
+      api_response = BBGAPI::geturl(partial,"")
+      api_response.each {|x|
+        puts "\n"
+        puts "Name:            #{x["backend_name"]}"
+        puts "ID:              #{x["id"]}"
+        puts "Alive Check:     #{x["monitoring_url"]}"
+        puts "Check Interval:  #{x["check_interval"]}"
+        nodes = x["lb_machines"].sort_by {|var| var["hostname"]}
+        nodes.each {|y|
+          puts "App:             #{y["hostname"]}"
+        }
+      }
+      puts "\n"
+      BBGAPI::parseopt("lb")
+    end
+
+    def self.tbi
+      puts "This is not yet implemented"
+      BBGAPI::parseopt("lb")
+    end
+
+  end
+end
